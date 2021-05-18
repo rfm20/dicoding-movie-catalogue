501361a7c6a58176a89cbc7fbf65fc7b305c7913
Rizki Firdaus <69286199+rfm20@users.noreply.github.com>
2021-05-18 15:50:49 +0700

    commit-3

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: '715ceb42374518ca931aa51620775532',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
