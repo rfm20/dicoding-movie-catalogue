0dde04f34c592fddac2acf57ccdf57d43247b651
Rizki Firdaus <69286199+rfm20@users.noreply.github.com>
2021-05-21 02:48:46 +0700

    commit-05

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    KEY: '715ceb42374518ca931aa51620775532',
+    KEY: 'YOUR_API_KEY',
     BASE_URL: 'https://api.themoviedb.org/3/',
     BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
     DEFAULT_LANGUAGE: 'en-us',
